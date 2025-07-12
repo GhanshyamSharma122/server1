7ec3cb788944cf355cfa0167e26b845ad9d6d938
shyam poudel <poudelshyam844@gmail.com>
2025-07-13 05:06:53 +0530

    feat: add post functionality for volunteers; implement photo upload and validation

--- src/controllers/volunteer.controller.js
+++ src/controllers/volunteer.controller.js
@@ -2,6 +2,9 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {Event} from "../models/event.model.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
+import {Post} from "../models/post.model.js"
+import Joi from "joi";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const rsvp=asyncHandler(async (req ,res)=>{
     const volunteer_id=req.user._id
@@ -25,6 +28,38 @@ const rsvp=asyncHandler(async (req ,res)=>{
 })
 const post=asyncHandler(async (req,res) => {
     const userId=req.user._id
+    const schema=Joi.object({
+        event_id:Joi.string().required(),
+        description:Joi.string().required(),
+    })
+    const {event_id,description}=req.body
+    const {error}=schema.validate(req.body)
+    if(error){
+        throw new ApiError(400,error.details[0].message)
+    }
+    const postPhotoLocalPath = req.file?.path
+        if (!postPhotoLocalPath) {
+            throw new ApiError(400, "avatar file is missing")
+        }
+        const photo = await uploadOnCloudinary(postPhotoLocalPath)
+        if (!photo.url) {
+            throw new ApiError(400, "error while uploading on avatar")
+        }
+    const post=await Post.create({
+        volunteer_id:req.user._id,
+        event_id,
+        description,
+        photoUrl:photo.url
+    })
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            post,
+            "post created succesfully"
+        )
+    )
     
 })
-export {rsvp}
+export {rsvp,post}
--- src/routes/volunteer.route.js
+++ src/routes/volunteer.route.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 import {roleMiddleware} from "../middlewares/role.middleware.js"
-import { rsvp } from "../controllers/volunteer.controller.js";
+import { post, rsvp } from "../controllers/volunteer.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const router=Router()
 router.post("/rsvp/:eventId",verifyJWT,roleMiddleware(["volunteer2","user"]),rsvp)
+router.post("/post",upload.single("photo"),verifyJWT,post)
 export default router
