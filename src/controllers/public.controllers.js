f0703434be489bdf7507e790cfca477dcbcc0fe7
shyam poudel <poudelshyam844@gmail.com>
2025-07-13 07:27:36 +0530

    feat: add stats endpoint; implement user and donor statistics retrieval

--- src/controllers/public.controllers.js
+++ src/controllers/public.controllers.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Post } from "../models/post.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { Donor } from "../models/donor.model.js";
 const getPosts=asyncHandler(async (req,res) => {
     const posts=await Post.find()
     return res
@@ -27,4 +29,21 @@ const eventGallery=asyncHandler(async (req,res)=>{
     )
 
 })
-export {getPosts,eventGallery}
+const stats=asyncHandler(async(req,res)=>{
+    const noOfBeneficiary=await User.find({type:"beneficiary"}).countDocuments();
+    const noOfVolunteersAndUsers=await User.find({$or:[{type:"volunteer2"},{"type":"user"}]}).countDocuments();
+    const donationAmount=await Donor.aggregate([
+  {
+    $group: {
+      _id: null,
+      total: { $sum: "$amount" }
+    }
+  }
+])
+return res
+.status(200)
+.json(
+    new ApiResponse(200,{donationAmount,noOfBeneficiary,noOfVolunteersAndUsers})
+)
+})
+export {getPosts,eventGallery,stats}
--- src/routes/public.route.js
+++ src/routes/public.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { eventGallery, getPosts } from "../controllers/public.controllers.js";
+import { eventGallery, getPosts, stats } from "../controllers/public.controllers.js";
 const router=Router()
 router.get("/event-gallery/:eventId",eventGallery)
 router.get("/getPosts",getPosts)
+router.get("/stats",stats)
 export default router
